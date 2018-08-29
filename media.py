e42ebccceda3a35a85dfceba4c8a628d31744d4f
PatrickAzevedo <patrick@siaze.com.br>
2018-08-29 19:37:38 -0300

    add media.py

---
+++ media.py
@@ -0,0 +1,10 @@
+m = int(0)
+x = int(input ("Digite um número: "))
+m = m+1
+x = x+ int(input ("Digite um numero: "))
+m = m+1
+x = x+ int(input ("Digite um numero: "))
+m = m+1
+print ("Soma total : ",x)
+Media = x/m
+print ("média : ",Media)
